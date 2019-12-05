ca507e20947322a56a583d8a28ab3d91ac817ac2
Clara Dios <clara.dios.diez@gmail.com>
2019-12-05 18:12:57 +0100

    add ingredients in a proper ul

--- js/create.js
+++ js/create.js
@@ -11,20 +11,24 @@ function getTotalPrice (iva = 10,...nums ) {
 function createCard (object) {   
     if(object) {
         const { id, name, price, description, image, ingredients } = object;
+        const defaultId = '1';
         const defaultName = 'plato';
         const defaultPrice = '-';
         const defaultDesc = 'Sin descripción';
         const defaultImg = 'https://via.placeholder.com/500x500';
-        const defaultIngredientes = 'No disponibles';
+        const defaultIngredientes = 'Ingredientes no disponibles';
+        let ingredientsList = '';         
+        if (ingredients) {ingredients.forEach(ing => ingredientsList +=`<li>${ing}</li>`)};
+  
      const result =   `
-        <li id=${id} class="card">
+        <li id=${id || defaultId} class="card">
             <h3 class="card__name">${name || defaultName}</h3>
             <p class="card__price">Precio: ${price || defaultPrice} €</p>
             <div class="card__image">
                 <img src="${image || defaultImg}" alt="${name}">
             </div>
             <p class="card__description">${description || defaultDesc}</p>
-            <p class="card__ingredients">ingredientes: ${ingredients || defaultIngredientes} </p>
+            <ul class="card__ingredients">${ingredientsList || defaultIngredientes} </ul>
         </li>`
         return result;
     } else {
