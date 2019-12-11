9d434f195e8607272f454e5f3746eefa8dd33e83
Clara Dios <clara.dios.diez@gmail.com>
2019-12-11 17:45:44 +0100

    cart working

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 import { printFoodList, listenCards, printCounterResult} from './print.js';
-import { getTotalPrice } from './create.js';
+import { getTotalPrice, createShoppingCard } from './create.js';
 import { createCard } from './create.js';
 import { callApi } from './service.js';
 let shopArray = [];
@@ -25,11 +25,14 @@ function addToShopArray(event) {
 
 function printShopingData(event) {
     console.log('hola');
-    addToShopArray(event);
     const list = document.querySelector('.cart__list');
-    printFoodList(list,);   //-> con el ul-cart, shopArray, createShopingCart()
-    getTotalPrice();   //-> con un array de números recogidos del shopArray, cLISTA
-    printCounterResult();
+    const priceArray = shopArray.map(item => item.price);
+    const iva = 10;
+    addToShopArray(event);
+    printFoodList(list,shopArray,createShoppingCard); 
+      //-> con el ul-cart, shopArray, createShopingCart()
+    const result = getTotalPrice(iva,priceArray);   //-> con un array de números recogidos del shopArray, cLISTA
+    printCounterResult(result);
 }
 
 printDataFood();
